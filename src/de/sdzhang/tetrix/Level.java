288a2a7d4f87b222854908c5cd69bfdce3341fe6
zhangsida <sida.zhang@ploin.de>
2013-11-27 11:17:03 +0100

    formatting
    Signed-off-by: zhangsida <sida.zhang@ploin.de>

--- src/de/sdzhang/tetrix/Level.java
+++ src/de/sdzhang/tetrix/Level.java
@@ -1,22 +1,17 @@
 package de.sdzhang.tetrix;
 
 public enum Level {
-	LEVEL0(1000),
-	LEVEL1(900),
-	LEVEL2(800),
-	LEVEL3(700),
-	LEVEL4(600),
-	LEVEL5(500),
-	LEVEL6(400),
-	LEVEL7(300),
-	LEVEL8(200);
+	LEVEL0(1000), LEVEL1(900), LEVEL2(800), LEVEL3(700), LEVEL4(600), LEVEL5(500), LEVEL6(400), LEVEL7(300), LEVEL8(200);
 	private int geschwindigkeit;
-	Level(int g){
-		geschwindigkeit=g;
+
+	Level(int g) {
+		geschwindigkeit = g;
 	}
+
 	public int getGeschwindigkeit() {
 		return geschwindigkeit;
 	}
+
 	public void setGeschwindigkeit(int geschwindigkeit) {
 		this.geschwindigkeit = geschwindigkeit;
 	}
